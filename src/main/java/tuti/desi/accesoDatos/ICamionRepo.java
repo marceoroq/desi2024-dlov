be904bac679504f663fe0643e79836c8151f5d0d
Marcelo Oroquieta <oroquieta.m@gmail.com>
2024-11-10 18:10:56 -0300

    creation of camion model

---
+++ src/main/java/tuti/desi/accesoDatos/ICamionRepo.java
@@ -0,0 +1,20 @@
+package tuti.desi.accesoDatos;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import tuti.desi.entidades.Camion;
+
+@Repository
+public interface ICamionRepo extends JpaRepository<Camion, String> {
+	
+	@Query("SELECT c FROM Camion c WHERE c.patente=?1")
+	Optional<Camion> findByPatente(String patenteCamion);
+	
+	@Query("DELETE FROM Camion c WHERE c.patente = ?1")
+	void deleteByPatente(String patenteCamion);
+	
+}
